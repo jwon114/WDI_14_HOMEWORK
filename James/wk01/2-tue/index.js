9ad702e02fb984cac13c1ca14c4c80f1c02f0928
jwon114 <jthgwong@gmail.com>
2017-11-28 15:55:31 +1100

    homework

--- James/wk01/2-tue/index.js
+++ James/wk01/2-tue/index.js
@@ -4,9 +4,15 @@ console.log(name);
 var choice = prompt("On the menu tonight: Steak, fruit salad, tofurkey, pork chops. What is your menu choice?");
 choice = choice.toUpperCase();
 
-if (choice === "FRUIT SALAD" || choice === "TOFURKEY") {
-	console.log("This cuisine is vegan friendly");
-} else {
-	console.log("Vegans beware!");
-}
+var menuOptions = ["STEAK", "FRUIT SALAD", "TOFURKEY", "PORK CHOPS"];
+var isOption = menuOptions.includes(choice);
 
+if (isOption) {
+	if (choice === "FRUIT SALAD" || choice === "TOFURKEY") {
+		console.log("This cuisine is vegan friendly");
+	} else {
+		console.log("Vegans beware!");
+	}
+} else {
+	console.log("Not a menu option!");
+}
