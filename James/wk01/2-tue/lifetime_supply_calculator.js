9dc11c4ee061d7fc1c83cb0a75c557abdc8ee79d
jwon114 <jthgwong@gmail.com>
2017-11-28 16:41:46 +1100

    homework

--- James/wk01/2-tue/lifetime_supply_calculator.js
+++ James/wk01/2-tue/lifetime_supply_calculator.js
@@ -2,6 +2,6 @@ var age = prompt("What is your age?");
 var max_age = 100;
 var estimated_amount = 5;
 
-var total = (max_age - age) * estimated_amount;
+var total = (max_age - age) * estimated_amount * 365;
 
 console.log("You will need " + total + " to last you until the ripe old age of " + max_age);
