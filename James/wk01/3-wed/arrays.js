3aa7fc3c93a174842e625dba6c2c691ae1f6548c
jwon114 <jthgwong@gmail.com>
2017-11-29 17:13:52 +1100

    arrays_29_wed_nov

--- James/wk01/3-wed/arrays.js
+++ James/wk01/3-wed/arrays.js
@@ -5,12 +5,13 @@ days_of_the_week.pop();
 // Add Sunday to the front of array
 days_of_the_week.unshift('Sunday');
 
-var days_of_the_week2 = [['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'], ['Saturday', 'Sunday']];
+console.log(days_of_the_week);
+
+var days_of_the_week = [['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'], ['Saturday', 'Sunday']];
 
 // Remove weekend array from days_of_the_week
-days_of_the_week2.pop();
+days_of_the_week.pop();
 // Assign days_of_the_week to sorted weekday array
-days_of_the_week2 = days_of_the_week2[0].sort();
+weekdays = days_of_the_week[0].sort();
 
-console.log(days_of_the_week);
-console.log(days_of_the_week2);
+console.log(weekdays);
