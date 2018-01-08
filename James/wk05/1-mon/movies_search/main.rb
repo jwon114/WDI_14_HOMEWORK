be5eec99b9c80b9fbadb35afeb808a4052c6a870
jwon114 <jthgwong@gmail.com>
2018-01-08 22:10:27 +1100

    style updates

--- James/wk05/1-mon/movies_search/main.rb
+++ James/wk05/1-mon/movies_search/main.rb
@@ -8,16 +8,26 @@ end
 
 get '/movies' do
 	result = HTTParty.get("http://omdbapi.com/?apikey=2f6435d9&t=#{params[:movie]}")
-	@movie_title = result["Title"]
-	@year = result["Year"]
-	@rated = result["Rated"]
-	@poster = result["Poster"]
-	@released = result["Released"]
-	@runtime = result["Runtime"]
-	@actors = result["Actors"]
-	@plot = result["Plot"]
-	@ratings = result["Ratings"]
-	# move images switching logic here and set the image url as a variable
+	@movie_data = result
+	
+	@movie_ratings = []
+	result["Ratings"].each do |rating|
+		case rating["Source"]
+		when "Internet Movie Database"
+			ratings_image = "<i class='fa fa-imdb fa-5x'></i>"
+		when "Rotten Tomatoes"
+			if rating["Value"].gsub("%", "").to_i >= 60
+				ratings_image = "<img class='ratings_icon' src='../images/fresh.png' alt=''>"
+			else 
+				ratings_image = "<img class='ratings_icon' src='../images/rotten.png' alt=''>"
+			end
+		when "Metacritic"
+			ratings_image = "<img class='ratings_icon' src='../images/Metacritic.png' alt=''>"
+		end
+
+		@movie_ratings << { :image => ratings_image, :value => rating["Value"] }
+	end
+
 	erb :movies
 end
 
