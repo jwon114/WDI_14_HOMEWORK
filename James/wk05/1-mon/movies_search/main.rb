7f45e13f8ad99f6e8dcb52e1ff23a2913523e357
jwon114 <jthgwong@gmail.com>
2018-01-08 23:14:56 +1100

    rating styles

--- James/wk05/1-mon/movies_search/main.rb
+++ James/wk05/1-mon/movies_search/main.rb
@@ -8,24 +8,29 @@ end
 
 get '/movies' do
 	result = HTTParty.get("http://omdbapi.com/?apikey=2f6435d9&t=#{params[:movie]}")
+
 	@movie_data = result
 	
 	@movie_ratings = []
 	result["Ratings"].each do |rating|
 		case rating["Source"]
-		when "Internet Movie Database"
-			ratings_image = "<i class='fa fa-imdb fa-5x'></i>"
-		when "Rotten Tomatoes"
-			if rating["Value"].gsub("%", "").to_i >= 60
-				ratings_image = "<img class='ratings_icon' src='../images/fresh.png' alt=''>"
-			else 
-				ratings_image = "<img class='ratings_icon' src='../images/rotten.png' alt=''>"
-			end
-		when "Metacritic"
-			ratings_image = "<img class='ratings_icon' src='../images/Metacritic.png' alt=''>"
+			when "Internet Movie Database"
+				ratings_image = "<i class='fa fa-imdb fa-5x'></i>"
+				ratings_value = "<i class='fa fa-star'></i>" + rating["Value"]
+			when "Rotten Tomatoes"
+				if rating["Value"].gsub("%", "").to_i >= 60
+					ratings_image = "<img class='ratings_icon' src='../images/fresh.png' alt=''>"
+					ratings_value = rating["Value"]
+				else 
+					ratings_image = "<img class='ratings_icon' src='../images/rotten.png' alt=''>"
+					ratings_value = rating["Value"]
+				end
+			when "Metacritic"
+				ratings_image = "<img class='ratings_icon' src='../images/Metacritic.png' alt=''>"
+				ratings_value = rating["Value"]
 		end
 
-		@movie_ratings << { :image => ratings_image, :value => rating["Value"] }
+		@movie_ratings << { :image => ratings_image, :value => ratings_value }
 	end
 
 	erb :movies
