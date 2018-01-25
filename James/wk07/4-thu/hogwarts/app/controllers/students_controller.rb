089b79d9a17d4d453426b0c38f2da8de80430588
jwon114 <jthgwong@gmail.com>
2018-01-25 17:58:10 +1100

    added new student enrolment

---
+++ James/wk07/4-thu/hogwarts/app/controllers/students_controller.rb
@@ -0,0 +1,28 @@
+class StudentsController < ApplicationController
+
+	def index
+		@students = Student.all
+	end
+
+	def show
+		@student = Student.find(params[:id])
+	end
+
+	def create
+		new_student = Student.new
+		new_student.name = params[:first_name] + " " + params[:last_name]
+		new_student.house_id = sorting_hat
+		new_student.img_url = params[:img_url]
+		
+		if new_student.save
+			redirect_to '/'
+		else
+			render :new
+		end
+	end
+
+	def sorting_hat
+		return rand(1..4)
+	end
+
+end
